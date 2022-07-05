d420f1f0a409d9a5cce75637a9e3962f151f7275
Jay Mathis <code@mathis.network>
2022-07-05 18:18:23 -0500

    update

--- packages/tickets/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var get_1 = require("./get");
-var find_1 = require("./find");
-var create_1 = require("./create");
-var update_1 = require("./update");
-exports["default"] = [find_1["default"], get_1["default"], create_1["default"], update_1["default"]];
